775b685d199efdcdac5318ba570eee45dc495326
Basoro <dentix.id@gmail.com>
2020-05-13 21:49:02 +0800

    Update

--- src/fungsi/koneksiDB.java
+++ src/fungsi/koneksiDB.java
@@ -40,8 +40,8 @@ public class koneksiDB {
                         "    ___) || | | |  | ||  _ <  ___) | | . \\ | | | || (_| || | | | / /| (_| |\n" +
                         "   |____/|___||_|  |_||_| \\_\\|____/  |_|\\_\\|_| |_| \\__,_||_| |_|/___|\\__,_|\n" +
                         "                                                                           \n\n"+
-                        "   PROUDLY POWERED BY Instalasi ICT RSUD H. Damanhuri - Barabai                  \n"+
-                        "   Version 20.02.2020 [Activated]                                   \n\n"+
+                        "   PROUDLY POWERED BY Instalasi RS Aura Syifa - Kediri                  \n"+
+                        "   Version 2020.05.15 [Activated]                                   \n\n"+
                         "                                                                           \n"+
                         "  Licensi yang dianut di software ini https://en.wikipedia.org/wiki/Aladdin_Free_Public_License \n"+
                         "  Informasi dan panduan bisa dicek di halaman https://github.com/mas-elkhanza/SIMRS-Khanza/wiki \n"+
