37e129f37ca17959d8129a34f2de0a75ef3d2996
basoro <dentix.id@gmail.com>
2021-05-28 14:55:13 +0800

    Update auto update ke repo Github

--- src/simrskhanza/SIMRSKhanza.java
+++ src/simrskhanza/SIMRSKhanza.java
@@ -5,6 +5,9 @@
  */
 package simrskhanza;
 
+import fungsi.koneksiDB;
+import java.io.FileInputStream;
+import java.util.Properties;
 import usu.widget.util.WidgetUtilities;
 
 /**
@@ -12,18 +15,39 @@ import usu.widget.util.WidgetUtilities;
  * @author khanzasoft
  */
 public class SIMRSKhanza {
-
+    private static final Properties propVer = new Properties();  
+    private static final Properties propDatabase = new Properties();  
+    public static String version;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        WidgetUtilities.invokeLater(() -> {
-           //frmUtama utama=frmUtama.getInstance();
-           //utama.isWall();
-           //utama.setIconImage(new javax.swing.ImageIcon( getClass(). getResource("/picture/home.PNG")).getImage());
-           splash utama=new splash();
-           utama.setVisible(true);
-       });
+        try {
+            propVer.loadFromXML(new FileInputStream("setting/version.xml"));
+            propDatabase.loadFromXML(new FileInputStream("setting/database.xml"));
+        } catch (Exception e) {
+            System.out.println("Notif Setting : "+e);
+        }
+        version = propVer.getProperty("VERSION");
+        if(propDatabase.getProperty("AUTOUPDATESISTEM").equals("aktif")){
+            try {
+                if (!Update.getLatestVersion().equals(version) ) {
+                    new UpdateInfo(Update.getWhatsNew());
+                } else {
+                    WidgetUtilities.invokeLater(() -> {
+                       splash utama=new splash();
+                       utama.setVisible(true);
+                    });                                 
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } else {
+            WidgetUtilities.invokeLater(() -> {
+               splash utama=new splash();
+               utama.setVisible(true);
+            });             
+        }
     }
     
 }
